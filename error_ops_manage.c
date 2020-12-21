7cbb986d1ed847a93c4de36295cb3abcab52d07a
JohnBogey <maxwellloganlovell@gmail.com>
2020-12-21 21:55:42 +0000

    code_add more errors

--- error_ops_manage.c
+++ error_ops_manage.c
@@ -1,19 +1,6 @@
 #include "monty.h"
 
 /**
- * file_open_error - function that returns error when file open fails
- * @filename: the name of the file to open
- *
- * Return: EXIT_FAILURE always
- */
-int file_open_error(char *filename)
-{
-	printf("hello world");
-	fprintf(stderr, "Error: Can't open file %s\n", filename);
-	return (EXIT_FAILURE);
-}
-
-/**
  * op_push_error - function that returns error when push fails
  * @line_number: line error occured
  *
