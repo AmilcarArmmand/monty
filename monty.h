b97610f8a34f18eca675b3e7563bd86e8941de02
AmilcarArmmand <armmand@gmail.com>
2020-12-22 14:51:04 +0000

    Changed op_code function names to match the names in the get_op_function variadic call function

--- monty.h
+++ monty.h
@@ -48,11 +48,11 @@ typedef struct instruction_s
 
 
 /* need function naming convention and rename below functions */
-void stack_push_node(stack_t **stack, unsigned int line_number);
+void op_push(stack_t **stack, unsigned int line_number);
 void op_pall(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
-void stack_pop(stack_t **stack, unsigned int line_number);
-void stack_swap_node(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
 void op_nop(stack_t **stack, unsigned int line_number);
 void op_sub(stack_t **stack, unsigned int line_number);
@@ -61,8 +61,8 @@ void op_mul(stack_t **stack, unsigned int line_number);
 void op_mod(stack_t **stack, unsigned int line_number);
 void op_pchar(stack_t **stack, unsigned int line_number);
 void op_pstr(stack_t **stack, unsigned int line_number);
-void stack_rotl(stack_t **stack, unsigned int line_number);
-void stack_rotr(stack_t **stack, unsigned int line_number);
+void op_rotl(stack_t **stack, unsigned int line_number);
+void op_rotr(stack_t **stack, unsigned int line_number);
 void op_stack(stack_t **stack, unsigned int line_number);
 void op_queue(stack_t **stack, unsigned int line_number);
 
