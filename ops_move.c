a3ab608811a006571bcd5cb6ae3b74c15b2a963b
AmilcarArmmand <armmand@gmail.com>
2021-01-01 00:47:44 +0000

    Refactored rotr

--- ops_move.c
+++ ops_move.c
@@ -27,9 +27,12 @@ void op_swap(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * op_rotl - top to bottom
+ * op_rotl - rotates the stack to the top
  * @stack: pointer to head of doubly linked list
  * @line_number: current line number
+ *
+ * Description: The top element of the stack becomes the last one, and
+ *              the second top element of the stack becomes the first one.
  * Return: address of new nodes or NULL on failure
  */
 void op_rotl(stack_t **stack, unsigned int line_number)
@@ -54,29 +57,33 @@ void op_rotl(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * op_rotr - bottom to top
+ * op_rotr - rotates the stack to the bottom
  * @stack: pointer to head of doubly linked list
  * @line_number: current line number
+ *
+ * Description: The last element of the stack becomes
+ *              the top element of the stack
  * Return: address of new nodes or NULL on failure
  */
 void op_rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *top, *bottom, *new_bottom, *temp;
+	stack_t *top, *bottom;
 
 	(void)line_number;
 
-	temp = *stack;
-	if (temp->next->next != NULL && temp->next != NULL)
-	{
-		temp = temp->next;
-		top = temp;
-		bottom = temp;
-		while (bottom->next != NULL)
-			bottom = bottom->next;
-		new_bottom = bottom->prev;
-		new_bottom->next = bottom->next;
-		bottom->next = top;
-		bottom->prev = top->prev;
-		top->prev = bottom;
-	}
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
+
 }
